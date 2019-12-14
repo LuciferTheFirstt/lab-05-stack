5836d07485a0ff8cc83701e3916eef398c507286
LuciferTheFirstt <dimonator67@gmail.com>
2019-12-14 18:32:58 +0300

    new

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/AL/lab05/lab-05-stack
+# Build directory: /home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/AL/lab05/lab-05-stack
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(CH "/home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/AL/lab05/lab-05-stack/test_stack")
--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,86 @@
-// Copyright 2018 Your Name <your_email>
+#include <iostream>
 
-#ifndef INCLUDE_HEADER_HPP_
-#define INCLUDE_HEADER_HPP_
 
-#endif // INCLUDE_HEADER_HPP_
+template <typename T>
+class stack
+{
+  struct Node
+    {
+        T value;
+        Node *next;
+    };
+   Node *top;
+public:
+  stack(): top(nullptr){};
+  stack(const stack&) = delete;
+  stack& operator=(const stack&) = delete;
+
+  void push(T&& value)
+   {
+     Node *node = new Node;
+     node->velue =p;
+     node->next=top;
+     top=node;
+   }
+
+  void push(const T& value)
+  {
+    Node *node = new Node;
+    node->value = value;
+    node->next = top;
+    top = node;
+  }
+
+  void pop()
+  {
+   T value = top->value;
+    Node *tmp = top;
+    top = top->next;
+    delete tmp;
+  }
+
+  const T& head() const
+  {
+   return top->value;
+  }
+
+  template <typename ... Args>
+
+  void push_emplace(Args&&... value)
+  {
+    for (auto p : std::initializer_list<T>{value...})
+    {
+        Node *node = new Node;
+        node->value = p;
+        node->next = top;
+        top = node;
+    }
+  }
+
+  void push(T&& value)
+  {
+    Node *node = new Node;
+    node->value = value;
+    node->next = top;
+    top = node;
+  }
+
+  T pop()
+  {
+   T value = top->value;
+    Node *tmp = top;
+    top = top->next;
+    delete tmp;
+    return value;
+  }
+
+  ~stack()
+  {
+    while (top)
+    {
+        Node *tmp = top;
+        top = top->next;
+        delete tmp;
+    }
+  }
+};
